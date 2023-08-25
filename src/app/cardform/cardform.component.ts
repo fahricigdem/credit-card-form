fafb94a840bb54f36bd2523f51a087da0662f649
fahri.cigdem <fahri.cigdem@atsonline.de>
2023-08-25 10:09:34 +0200

    - create a emthy cardform.component

---
+++ src/app/cardform/cardform.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardform',
+  templateUrl: './cardform.component.html',
+  styleUrls: ['./cardform.component.css']
+})
+export class CardformComponent {
+
+}
