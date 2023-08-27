f5393051f01594b4a5564876ae784bd37f52a4f6
fahri.cigdem <fahri.cigdem@atsonline.de>
2023-08-27 15:29:00 +0200

    - create Input Component
     - add other fields into form group

--- src/app/cardform/cardform.component.ts
+++ src/app/cardform/cardform.component.ts
@@ -9,18 +9,43 @@ import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 export class CardformComponent implements OnInit{
 
-  cardForm = new FormGroup({
-    name:new FormControl(null, [
-      Validators.required,
-      Validators.minLength(4),
-    ])
-  })
+  cardForm = new FormGroup(
+    {
+      name:new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      cardNumber:new FormControl(null, [
+        Validators.required,
+        Validators.minLength(16),
+        Validators.maxLength(16),
+      ]),
+      lastDate:new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(4),
+      ]),
+      securityNumber:new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(3),
+      ])
+
+    }
+  )
 
   ngOnInit(){
     console.log(this.cardForm);
   }
 
+  onSubmit(){
+    console.log("submitted!\\n");
+    console.table(this.cardForm.value);
+  }
 
   get name() { return this.cardForm.get('name'); }
+  get cardNumber() { return this.cardForm.get('cardNumber'); }
+  get lastDate() { return this.cardForm.get('lastDate'); }
+  get securityNumber() { return this.cardForm.get('securityNumber'); }
 
 }
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit,Input } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  @Input() control! : FormControl;
+  @Input() label! : string;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.control);
+  }
+
+  isRequired(){
+    return this.control?.errors?.['required'] == true && this.control?.touched;
+  }
+  isMinLength(){
+    return this.control?.errors?.['minlength'] != null && this.control?.touched;
+  }
+  isMaxLength(){
+    return this.control?.errors?.['maxlength'] != null && this.control?.touched;
+  }
+
+}
